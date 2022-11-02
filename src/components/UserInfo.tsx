dd89857ad0eed88c5dfd0360c370632f2f5df37b
Michal Rusek <michal.rusek@dka.io>
2022-11-02 20:50:32 +0100

    add to the slider images and map it

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -21,26 +21,6 @@ const UserInfo: React.FC<UserInfoProps> = ({}) => {
         <>
           <div className="movies">
             <FilmSlider />
-            <div className="movieInfo"> </div>
-            <text> title</text>
-            <text> rating</text>
-            <text>
-              description Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Officia commodi dicta labore magni quo id eligendi!
-              Excepturi quo voluptatem quaerat ea, tenetur quibusdam commodi
-              ratione, eveniet iure eaque soluta iusto! Lorem ipsum dolor sit
-              amet consectetur adipisicing elit. Inventore sapiente ducimus
-              incidunt assumenda modi recusandae hic, ex similique alias
-              distinctio ipsam vel. Tenetur, consectetur exercitationem
-              reiciendis aliquid cumque qui nesciunt. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Cum aspernatur odio est nemo odit
-              ducimus, nihil praesentium labore debitis enim quidem consectetur
-              sequi ipsam quis vitae eius! Nam, totam esse? Lorem ipsum dolor,
-              sit amet consectetur adipisicing elit. Quis laudantium corporis
-              sapiente modi minus quibusdam ipsam nemo provident! Corporis
-              nostrum, animi odio rem hic id maxime laborum pariatur officia
-              voluptatem?
-            </text>
           </div>
         </>
       )}
