bc26c14684571b959f89c755695c745fef90978d
Bartekxseo <Barteo55@wp.pl>
2022-11-20 13:30:08 +0100

    added empty controlles to generate swagger file for frontend

---
+++ FilmAppka - BackEnd/FA.RestApi/Controllers/MovieController.cs
@@ -0,0 +1,60 @@
+﻿using FA.Services.Models;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FA.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public MovieController()
+        {
+        }
+
+        [HttpGet("getAllMovies")]
+        public List<MovieInfo> getAllMovies()
+        {
+
+            return new List<MovieInfo>();
+            //try
+            //{
+            //    return userService.getShelter(shelterId);
+            //}
+            //catch (Exception ex)
+            //{
+            //    logger.Error(ex);
+            //    throw new Exception("", ex);
+            //}
+        }
+
+        [HttpGet("getWatchedMoviesForUser")]
+        public List<MovieInfo> getWatchedMoviesForUser(int userId)
+        {
+            return new List<MovieInfo>();
+        }
+        [HttpPatch("addMovieAsWatched")]
+        public void addMovieAsWatched(int userId,int movieId)
+        {
+
+        }
+        [HttpDelete("removeMovieFromWatched")]
+        public void removeMovieFromWatched(int userId, int movieId)
+        {
+
+        }
+        [HttpGet("getMovieSugestions")]
+        public List<MovieInfo> getMovieSugestions(int userId, string mood)
+        {
+            return new List<MovieInfo>();
+        }
+    }
+}
---
+++ FilmAppka - BackEnd/FA.RestApi/Controllers/TypesController.cs
@@ -0,0 +1,60 @@
+﻿using FA.Services.Models;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FA.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypesController : ControllerBase
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public TypesController()
+        {
+        }
+
+        [HttpPatch("addLikedMovieType")]
+        public void addLikedMovieType(int userId,int typeId)
+        {
+
+        }
+        [HttpPatch("addDislikedMovieType")]
+        public void addDislikedMovieType(int userId, int typeId)
+        {
+
+        }
+        [HttpDelete("removeLikedMovieType")]
+        public void removeLikedMovieType(int userId, int typeId)
+        {
+
+        }
+        [HttpDelete("removeDislikedMovieType")]
+        public void removeDislikedMovieType(int userId, int typeId)
+        {
+
+        }
+        [HttpGet("getAllMovieTypes")]
+        public List<TypeInfo> getAllMovieTypes()
+        {
+            return new List<TypeInfo>();
+        }
+        [HttpGet("getLikedMovieTypes")]
+        public List<TypeInfo> getLikedMovieTypes(int userId)
+        {
+            return new List<TypeInfo>();
+        }
+        [HttpGet("getDislikedMovieTypes")]
+        public List<TypeInfo> getDislikedMovieTypes(int userId)
+        {
+            return new List<TypeInfo>();
+        }
+
+    }
+}
