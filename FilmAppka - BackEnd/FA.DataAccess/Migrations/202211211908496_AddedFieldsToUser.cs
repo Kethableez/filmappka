348d06b7dcc43a798d40f71cdd1c26215b4fe6ad
Bartekxseo <Barteo55@wp.pl>
2022-11-21 22:08:40 +0100

    Poprawka namespaców

---
+++ FilmAppka - BackEnd/FA.DataAccess/Migrations/202211211908496_AddedFieldsToUser.cs
@@ -0,0 +1,24 @@
+﻿namespace FA.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedFieldsToUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.User", "FirstName", c => c.String(nullable: false));
+            AddColumn("dbo.User", "UserName", c => c.String(nullable: false));
+            AddColumn("dbo.User", "Email", c => c.String(nullable: false));
+            DropColumn("dbo.User", "Name");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.User", "Name", c => c.String(nullable: false));
+            DropColumn("dbo.User", "Email");
+            DropColumn("dbo.User", "UserName");
+            DropColumn("dbo.User", "FirstName");
+        }
+    }
+}
