9aa672c8ef201f57e65e02fdfdac875aea749948
Bartosz <Bartosz@dsr.com.pl>
2022-12-21 15:17:06 +0100

    migracja

---
+++ FilmAppka - BackEnd/FA.DataAccess/Migrations/202212211416046_addLastKnownEmotionToUser.cs
@@ -0,0 +1,18 @@
+﻿namespace FA.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addLastKnownEmotionToUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.User", "LastKnownEmotion", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.User", "LastKnownEmotion");
+        }
+    }
+}
