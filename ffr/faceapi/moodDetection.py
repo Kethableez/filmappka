1472cdbb5fbeac54072e787d825ad63fc614efd5
Kethableez <mocarto98@gmail.com>
2023-01-16 21:53:46 +0100

    added fer instead deepface

--- ffr/faceapi/moodDetection.py
+++ ffr/faceapi/moodDetection.py
@@ -1,10 +1,12 @@
-from deepface import DeepFace as DF
+from fer import FER
 import cv2
 
 def detectMood(buff):
   try:
     img = cv2.imdecode(buff, cv2.IMREAD_ANYCOLOR)
-    predictions = DF.analyze(img)
-    return predictions['emotion']
+    detector = FER()
+    predictions = detector.detect_emotions(img)
+    print(predictions[0]['emotions'])
+    return predictions[0]['emotions']
   except Exception as e:
     raise Exception('[FFR - MDT] No face were recognised')
