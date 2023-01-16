d152cba338a2f5d61dce1bf6d28cb41013e5b055
Kethableez <mocarto98@gmail.com>
2023-01-16 21:32:07 +0100

    updated run.sh

--- ffr/bin/run.sh
+++ ffr/bin/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gunicorn wsgi:app --bind 0.0.0.0:9000 --log-level=debug --workers=4 --timeout=120
+gunicorn wsgi:app --bind 0.0.0.0:9000 --log-level=debug --workers=4 --timeout=120 --graceful-timeout=120
